985e27e9af8d93f36d68ae5c66afed7e760b65d8
cswu <xray0h@gmail.com>
2017-04-20 11:04:49 +0800

    shorter function

--- homework/hw2.py
+++ homework/hw2.py
@@ -1,28 +1,22 @@
 import random
 
 # HW0002：
-
 # 1. 產生五個亂數，並將其輸出。
 
-x1 = []                                 # 宣告 list
-for idx in range(5):
-    x1.append(random.random())          # 產生隨機變數，附加到list後面
+x1 = [random.random() for idx in range(5)]
+
 print(x1)
 
 # 2. 產生N個介於-1與1之間的亂數，計算其平均值與標準差並輸出，每個亂數的值則不用輸出。N=10^1, 10^2, 10^3, 10^4, 10^5。
 
-x2 = []
-for idx in range(5):
-    x2.append(random.uniform(-1, 1))     # 產生uniform分布的隨機變數，附加到list後面
+x2 = [random.uniform(-1,1) for idx in range(5)]
 print(x2)
 
 
 # 3. 做基本題2時，一併輸出產生每N個亂數前後的系統時間，並計算所需的時間。
 
 def func():                               #宣告函數
-    x2 = []
-    for idx in range(1000000):
-        x2.append(random.uniform(-1, 1))
+    x2 = [random.uniform(-1,1) for idx in range(1000000)]
 
 import time                                #引入time
 start_time = time.time()
